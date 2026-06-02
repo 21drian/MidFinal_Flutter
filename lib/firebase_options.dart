4e82b578dea5d7072823e14e924ec982fb9ca31d
Adrian Paul Macalolot <adrianpaul201211@gmail.com>
2026-06-02 10:19:07 +0800

    Update .dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 
-/// Firebase configuration for the Boiser app.
+///
 ///
 /// ════════════════════════════════════════════════════════════════
 ///  HOW TO FILL THIS IN
